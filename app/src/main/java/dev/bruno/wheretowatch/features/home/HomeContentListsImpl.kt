d6df6e4845e24ee8154eae19b2c81a74dbb64a28
brunohensel <bruno.hensel@gmail.com>
2023-12-06 17:19:50 +0100

    add upcoming movie flow source

--- app/src/main/java/dev/bruno/wheretowatch/features/home/HomeContentListsImpl.kt
+++ app/src/main/java/dev/bruno/wheretowatch/features/home/HomeContentListsImpl.kt
@@ -3,6 +3,7 @@ package dev.bruno.wheretowatch.features.home
 import com.squareup.anvil.annotations.ContributesBinding
 import dev.bruno.wheretowatch.di.AppScope
 import dev.bruno.wheretowatch.features.home.movies.PopularMovieFlowSource
+import dev.bruno.wheretowatch.features.home.movies.UpComingMovieFlowSource
 import dev.bruno.wheretowatch.features.home.trending.TrendingFlowSource
 import javax.inject.Inject
 
@@ -10,18 +11,21 @@ import javax.inject.Inject
 class HomeContentListsImpl @Inject constructor(
     private val trendingSource: TrendingFlowSource,
     private val popularSource: PopularMovieFlowSource,
+    private val upComingSource: UpComingMovieFlowSource,
 ) : HomePresenter.HomeContentLists {
 
     override val contents: HomeContentFlows
         get() = HomeContentFlows(
             trendingContent = trendingSource.flow,
             popularContent = popularSource.flow,
+            upComingContent = upComingSource.flow,
         )
 
     override suspend fun getContent(contentType: HomeContentType) {
         when (contentType) {
             is HomeContentType.Trending -> trendingSource.getTrending(contentType.window)
             HomeContentType.Popular -> popularSource.getPopular()
+            HomeContentType.UpComing -> upComingSource.getUpComing()
         }
     }
 }
