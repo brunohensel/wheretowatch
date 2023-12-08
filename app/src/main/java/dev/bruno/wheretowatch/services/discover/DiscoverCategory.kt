7f597828612449391847295358373ae9df03c8db
brunohensel <bruno.hensel@gmail.com>
2023-12-08 18:50:49 +0100

    add supported category for request

--- app/src/main/java/dev/bruno/wheretowatch/services/discover/DiscoverCategory.kt
+++ app/src/main/java/dev/bruno/wheretowatch/services/discover/DiscoverCategory.kt
@@ -8,3 +8,25 @@ sealed interface DiscoverCategory {
 }
 
 enum class TrendWindow(val key: String) { DAY("day"), WEEK("week") }
+
+enum class MovieGenre(val key: String) {
+    ACTION("Action"),
+    ADVENTURE("Adventure"),
+    ANIMATION("Animation"),
+    COMEDY("Comedy"),
+    CRIME("Crime"),
+    DOCUMENTARY("Documentary"),
+    DRAMA("Drama"),
+    FAMILY("Family"),
+    FANTASY("Fantasy"),
+    HISTORY("History"),
+    HORROR("Horror"),
+    MUSIC("Music"),
+    MYSTERY("Mystery"),
+    ROMANCE("Romance"),
+    SCIENCE_FICTION("Science Fiction"),
+    TV_MOVIE("TV Movie"),
+    THRILLER("Thriller"),
+    WAR("War"),
+    WESTERN("Western"),
+}
