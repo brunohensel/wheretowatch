8741e044b469a3100becbac22f4a494b783f15c0
brunohensel <bruno.hensel@gmail.com>
2023-12-08 16:16:04 +0100

    refactor TrendWindow type

--- app/src/main/java/dev/bruno/wheretowatch/features/home/HomeContentType.kt
+++ app/src/main/java/dev/bruno/wheretowatch/features/home/HomeContentType.kt
@@ -1,9 +1,9 @@
 package dev.bruno.wheretowatch.features.home
 
-import dev.bruno.wheretowatch.services.trending.TrendingSupplier
+import dev.bruno.wheretowatch.services.discover.TrendWindow
 
 sealed interface HomeContentType {
-    data class Trending(val window: TrendingSupplier.TrendWindow) : HomeContentType
+    data class Trending(val window: TrendWindow) : HomeContentType
     data object Popular : HomeContentType
     data object Upcoming : HomeContentType
     data object TopRated : HomeContentType
