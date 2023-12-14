8f0aecd9a9b64f287a6c50a1a071759d30e4adbf
brunohensel <bruno.hensel@gmail.com>
2023-12-14 21:58:41 +0100

    add material icons library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.iconsExtended)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.datastore.preferences.core)
     implementation(libs.circuit.circuitx)
