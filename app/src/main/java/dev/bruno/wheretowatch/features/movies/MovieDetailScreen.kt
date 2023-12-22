7e9db9ad2b4c01076e1b0adc5bb7092065e05245
brunohensel <bruno.hensel@gmail.com>
2023-12-22 19:16:08 +0100

    get movie details e present it as state to ui

--- app/src/main/java/dev/bruno/wheretowatch/features/movies/MovieDetailScreen.kt
+++ app/src/main/java/dev/bruno/wheretowatch/features/movies/MovieDetailScreen.kt
@@ -1,18 +1,22 @@
 package dev.bruno.wheretowatch.features.movies
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
 import dev.bruno.wheretowatch.di.AppScope
-import dev.bruno.wheretowatch.services.model.Movie
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class MovieDetailScreen(val movieId: Int) : Screen {
     data class State(
-        val movie: Movie = Movie(),
+        val movie: MovieDetailsItem,
     ) : CircuitUiState
 }
 
@@ -22,5 +26,14 @@ fun MovieDetailContent(
     state: MovieDetailScreen.State,
     modifier: Modifier = Modifier,
 ) {
-
+    Scaffold(modifier = modifier) { paddingValues ->
+        LazyColumn(contentPadding = paddingValues) {
+            item(key = "backdropBox") {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    val movie = state.movie
+                    Text(text = movie.title)
+                }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/dev/bruno/wheretowatch/services/movies/detail/MovieDetailsSupplier.kt
@@ -0,0 +1,56 @@
+package dev.bruno.wheretowatch.services.movies.detail
+
+import com.squareup.anvil.annotations.ContributesBinding
+import dev.bruno.wheretowatch.di.AppScope
+import dev.bruno.wheretowatch.ds.components.ImageType
+import dev.bruno.wheretowatch.features.movies.MovieDetailsItem
+import dev.bruno.wheretowatch.services.discover.DiscoveryImageModel
+import dev.bruno.wheretowatch.services.model.MovieWithDetails
+import javax.inject.Inject
+
+interface MovieDetailsSupplier {
+    suspend fun get(movieId: Int): MovieDetailsItem
+}
+
+@ContributesBinding(AppScope::class)
+class MovieDetailStore @Inject constructor(
+    private val movieDetailsRemote: MovieDetailsRemote,
+    private val movieDetailDao: MovieDetailDao,
+) : MovieDetailsSupplier {
+    override suspend fun get(movieId: Int): MovieDetailsItem {
+        val localMovieDetail = movieDetailDao.getMovieDetails(movieId)
+        if (localMovieDetail == null) {
+            movieDetailsRemote.fetchMovieDetail(movieId)
+                .let { dto -> movieDetailDao.insertMovieDetails(dto) }
+
+            return movieDetailDao.getMovieDetails(movieId).toMovieDetailItem()
+        }
+        return localMovieDetail.toMovieDetailItem()
+    }
+
+    private fun MovieWithDetails?.toMovieDetailItem(): MovieDetailsItem {
+        requireNotNull(this)
+        return MovieDetailsItem(
+            id = id,
+            title = title,
+            overview = overview,
+            originalTitle = originalTitle,
+            popularity = popularity,
+            voteCount = voteCount,
+            voteAverage = voteAverage,
+            genresIds = genresIds,
+            releaseDate = releaseDate,
+            homePage = homePage,
+            budget = budget,
+            revenue = revenue,
+            runtime = runtime,
+            tagline = tagline,
+            collectionId = collectionId,
+            buildImgModel = partialImgModel()
+        )
+    }
+
+    private fun MovieWithDetails.partialImgModel(): (ImageType) -> DiscoveryImageModel = { type ->
+        DiscoveryImageModel(backdropPath = backdropPath, posterPath = posterPath, type)
+    }
+}
