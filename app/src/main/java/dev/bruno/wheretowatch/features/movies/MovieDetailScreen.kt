b9fda10deae10944337a5cab433a5dc56ab6a1f7
brunohensel <bruno.hensel@gmail.com>
2023-12-27 15:08:07 +0100

    add movie info.

--- app/src/main/java/dev/bruno/wheretowatch/features/movies/MovieDetailScreen.kt
+++ app/src/main/java/dev/bruno/wheretowatch/features/movies/MovieDetailScreen.kt
@@ -1,5 +1,6 @@
 package dev.bruno.wheretowatch.features.movies
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,19 +9,29 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiState
@@ -58,10 +69,11 @@ fun MovieDetailContent(
                                     drawContent()
                                     drawRect(
                                         Brush.verticalGradient(
-                                        0.8f to Color.Black.copy(alpha=.0F),
-                                        0.9f to Color.Black.copy(alpha=.2F),
-                                        1F to Color.Black.copy(alpha=.6F),
-                                    ))
+                                            0.8f to Color.Black.copy(alpha = .0F),
+                                            0.9f to Color.Black.copy(alpha = .2F),
+                                            1F to Color.Black.copy(alpha = .6F),
+                                        )
+                                    )
                                 }
                             }
                             .fillMaxWidth()
@@ -95,14 +107,128 @@ fun MovieDetailContent(
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     Column {
-                        Text(
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                            text = movie.title,
+                        InfoRow(
+                            rowTitle = "Release date",
+                            rowInfo = movie.releaseDate?.toString() ?: ""
                         )
+                        val budget = "%,d".format(movie.budget)
+                        InfoRow(rowTitle = "Budget", rowInfo = budget)
+
+                        val revenue = "%,d".format(movie.revenue)
+                        InfoRow(rowTitle = "Revenue", rowInfo = revenue)
+
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            style = MaterialTheme.typography.labelLarge,
+                            text = "Tmdb Rating",
+                        )
+                        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                            Row {
+                                Text(
+                                    style = MaterialTheme.typography.labelLarge,
+                                    text = "User score :",
+                                    modifier = Modifier
+                                        .align(Alignment.CenterVertically)
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                UserScore(movie.voteAverage)
+                                Spacer(modifier = Modifier.width(8.dp))
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            val voteCount = "%,d".format(movie.voteCount)
+                            InfoRow(rowTitle = "Vote count", rowInfo = voteCount)
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun InfoRow(
+    rowTitle: String,
+    rowInfo: String,
+) {
+    Row {
+        Text(
+            style = MaterialTheme.typography.labelLarge,
+            text = "$rowTitle :",
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            style = MaterialTheme.typography.labelMedium,
+            text = rowInfo,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@Composable
+private fun UserScore(
+    voteAverage: Float,
+    modifier: Modifier = Modifier,
+    strokeWidth: Dp = 4.dp,
+    strokeCap: StrokeCap = StrokeCap.Square,
+) {
+    val stroke = with(LocalDensity.current) {
+        Stroke(width = strokeWidth.toPx(), cap = strokeCap)
+    }
+
+    // the vote average is represented as floating point (ex 7.2)
+    // We need it as percentage (ex. 0.72) to ease the calculation of the sweep and also to get
+    // the integer from it to draw a Text inside the circle (ex. 72%)
+    val averagePercent = voteAverage / 10
+    val text = "${"%.0f".format(averagePercent * 100)}%"
+    val style = MaterialTheme.typography
+        .bodySmall.copy(
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold
+        )
+    val textMeasure = rememberTextMeasurer()
+    val measureResult = remember(text) { textMeasure.measure(text, style) }
+    val color = if (averagePercent >= 0.65) {
+        MaterialTheme.colorScheme.primary
+    } else {
+        Color.Yellow // TODO get this color from design system or from light/dark palette
+    }
+
+    Canvas(
+        modifier = modifier.size(measureResult.size.width.dp / 2),
+    ) {
+        val diameterOffset = stroke.width / 2
+        val arcDimen = size.width - 2 * diameterOffset
+        drawArc(
+            color = color.copy(alpha = .5f),
+            startAngle = 0f,
+            sweepAngle = 360f,
+            useCenter = false,
+            topLeft = Offset(diameterOffset, diameterOffset),
+            size = Size(arcDimen, arcDimen),
+            style = stroke
+        )
+
+        val sweep = averagePercent * 360
+        drawArc(
+            color = color,
+            startAngle = -90f,
+            sweepAngle = sweep,
+            useCenter = false,
+            topLeft = Offset(diameterOffset, diameterOffset),
+            size = Size(arcDimen, arcDimen),
+            style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+        )
+
+        drawText(
+            textMeasurer = textMeasure,
+            text = text,
+            style = style,
+            topLeft = Offset(
+                x = center.x - measureResult.size.width / 2,
+                y = center.y - measureResult.size.height / 2
+            )
+        )
     }
 }
