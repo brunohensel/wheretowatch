68862c05889a0830db9e3a3ffcd9dadeb271318a
brunohensel <bruno.hensel@gmail.com>
2023-12-27 11:55:09 +0100

    add tagline to backdrop image.

--- app/src/main/java/dev/bruno/wheretowatch/features/movies/MovieDetailScreen.kt
+++ app/src/main/java/dev/bruno/wheretowatch/features/movies/MovieDetailScreen.kt
@@ -1,12 +1,26 @@
 package dev.bruno.wheretowatch.features.movies
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiState
@@ -33,24 +47,61 @@ fun MovieDetailContent(
         LazyColumn(contentPadding = paddingValues) {
             val movie = state.movie
             item(key = "backdropBox") {
+                Box {
+                    AndroidAsyncImage(
+                        model = movie.buildImgModel(ImageType.Backdrop),
+                        contentDescription = "Movie detail backdrop image",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .drawWithCache {
+                                onDrawWithContent {
+                                    drawContent()
+                                    drawRect(
+                                        Brush.verticalGradient(
+                                        0.8f to Color.Black.copy(alpha=.0F),
+                                        0.9f to Color.Black.copy(alpha=.2F),
+                                        1F to Color.Black.copy(alpha=.6F),
+                                    ))
+                                }
+                            }
+                            .fillMaxWidth()
+                            .aspectRatio(16f / 11)
+                    )
 
-                AndroidAsyncImage(
-                    model = movie.buildImgModel(ImageType.Backdrop),
-                    contentDescription = "Movie detail backdrop image",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(16f / 11)
-                )
+                    Text(
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold,
+                        text = movie.tagline,
+                        modifier = Modifier
+                            .padding(end = 4.dp)
+                            .align(Alignment.BottomEnd)
+                    )
+                }
             }
 
             item(key = "poster") {
-                AndroidAsyncImage(
-                    model = movie.buildImgModel(ImageType.Poster),
-                    contentDescription = "Movie detail poster image",
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
                     modifier = Modifier
-                        .width(150.dp)
-                        .aspectRatio(2f / 3)
-                )
+                        .padding(horizontal = 16.dp)
+                ) {
+                    AndroidAsyncImage(
+                        model = movie.buildImgModel(ImageType.Poster),
+                        contentDescription = "Movie detail poster image",
+                        modifier = Modifier
+                            .width(150.dp)
+                            .aspectRatio(2f / 3)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Column {
+                        Text(
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold,
+                            text = movie.title,
+                        )
+                    }
+                }
             }
         }
     }
