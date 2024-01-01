5924a79d011af81406d9794733313d6d270eaef5
Bruno Hensel <bruno.hensel@gmail.com>
2024-01-01 16:45:55 +0100

    use `DiscoverContentSupplier` directly instead of a shallow class.

--- app/src/main/java/dev/bruno/wheretowatch/features/discover/DiscoverContentListsImpl.kt
+++ app/src/main/java/dev/bruno/wheretowatch/features/discover/DiscoverContentListsImpl.kt
@@ -12,29 +12,33 @@ import dev.bruno.wheretowatch.features.discover.DiscoverContentType.Trending
 import dev.bruno.wheretowatch.features.discover.DiscoverContentType.Upcoming
 import dev.bruno.wheretowatch.features.discover.DiscoverContentType.War
 import dev.bruno.wheretowatch.features.discover.movies.CollectionMovieSource
-import dev.bruno.wheretowatch.features.discover.movies.PopularMovieSource
 import dev.bruno.wheretowatch.features.discover.movies.StreamProviderMovieSource
 import dev.bruno.wheretowatch.features.discover.movies.TopRatedSource
 import dev.bruno.wheretowatch.features.discover.movies.TrendingMovieSource
 import dev.bruno.wheretowatch.features.discover.movies.UpcomingMovieSource
+import dev.bruno.wheretowatch.services.discover.DiscoverCategory
+import dev.bruno.wheretowatch.services.discover.DiscoverContentSupplier
 import dev.bruno.wheretowatch.services.discover.MovieCollection.HARRY_POTTER
 import dev.bruno.wheretowatch.services.discover.MovieGenre
 import dev.bruno.wheretowatch.services.discover.StreamerProvider
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
+import dev.bruno.wheretowatch.services.model.DiscoverContent as DiscoverContentModel
 
 @ContributesBinding(AppScope::class)
 class DiscoverContentListsImpl @Inject constructor(
     private val trendingSource: TrendingMovieSource,
-    private val popularSource: PopularMovieSource,
     private val upcomingSource: UpcomingMovieSource,
     private val streamSource: StreamProviderMovieSource,
     private val topRatedSource: TopRatedSource,
     private val collectionSource: CollectionMovieSource,
+    private val supplier: DiscoverContentSupplier,
 ) : DiscoverPresenter.HomeContentLists {
 
     private val feedMap = ConcurrentHashMap<DiscoverSections, DiscoverContent>()
@@ -56,7 +60,7 @@ class DiscoverContentListsImpl @Inject constructor(
                 updateSorted()
             }
 
-            Popular -> popularSource.get()
+            Popular -> getPopularMovieContent()
                 .update(section = DiscoverSections.Popular)
 
             Upcoming -> upcomingSource.get()
@@ -65,21 +69,45 @@ class DiscoverContentListsImpl @Inject constructor(
             TopRated -> topRatedSource.get()
                 .update(section = DiscoverSections.TopRated)
 
-            Action -> popularSource.get(MovieGenre.ACTION)
+            Action -> getPopularMovieContent(MovieGenre.ACTION)
                 .update(section = DiscoverSections.Action)
 
-            Horror -> popularSource.get(MovieGenre.HORROR)
+            Horror -> getPopularMovieContent(MovieGenre.HORROR)
                 .update(section = DiscoverSections.Horror)
 
             Netflix -> streamSource.get(StreamerProvider.NETFLIX)
                 .update(section = DiscoverSections.Netflix)
 
-            War -> popularSource.get(MovieGenre.WAR)
+            War -> getPopularMovieContent(MovieGenre.WAR)
                 .update(section = DiscoverSections.War)
 
             HarryPotterCollection -> collectionSource.get(HARRY_POTTER)
                 .update(section = DiscoverSections.HarryPotter)
         }
+    }
+
+    private suspend fun getPopularMovieContent(
+        genre: MovieGenre = MovieGenre.ALL,
+    ): DiscoverContent {
+        return supplier
+            .get(DiscoverCategory.Popular(genre))
+            .asDiscoverMovieItem()
+    }
+
+    private fun ImmutableList<DiscoverContentModel>.asDiscoverMovieItem(): DiscoverContent {
+        val items = this.map { item ->
+            DiscoverMovieItem(
+                id = item.id,
+                title = item.title,
+                originalTitle = item.originalTitle,
+                popularity = item.popularity,
+                voteAverage = item.voteAverage,
+                voteCount = item.voteCount,
+                buildImgModel = item.curried()
+            )
+        }.toImmutableList()
+
+        return ContentList(items)
     }
 
     private fun DiscoverContent.update(section: DiscoverSections) {
