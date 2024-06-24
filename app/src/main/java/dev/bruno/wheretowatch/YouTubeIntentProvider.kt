0edb03de7ded99c112a41e6af222694801e723e8
Bruno Hensel <bruno.hensel@gmail.com>
2024-06-24 17:48:57 +0200

    replace anonymous function with regular one

--- app/src/main/java/dev/bruno/wheretowatch/YouTubeIntentProvider.kt
+++ app/src/main/java/dev/bruno/wheretowatch/YouTubeIntentProvider.kt
@@ -7,10 +7,8 @@ object YouTubeIntentProvider {
     private const val YOUTUBE_PACKAGE = "com.google.android.youtube"
     private const val YOUTUBE_URL = "https://www.youtube.com/watch?v=%s"
 
-    val get = { key: String ->
-        Intent(Intent.ACTION_VIEW).apply {
-            setPackage(YOUTUBE_PACKAGE)
-            setData(Uri.parse(YOUTUBE_URL.format(key)))
-        }
+    fun get(key: String) = Intent(Intent.ACTION_VIEW).apply {
+        setPackage(YOUTUBE_PACKAGE)
+        setData(Uri.parse(YOUTUBE_URL.format(key)))
     }
 }
